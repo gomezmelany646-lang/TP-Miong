44c8c437b7a0162c62735efd82ae51973e01c75b
Agustin Castillo <agustinezequielcastillo100@gmail.com>
2026-07-07 03:55:33 -0300

    agregue boton de volver arriba con JS en index y contacto

--- js/main.js
+++ js/main.js
@@ -8,3 +8,11 @@ function toggleOscuro() {
         btnOscuro.textContent = '🌑 Modo oscuro'
     }
 }
+
+// BOTON VOLVER ARRIBA
+const btnArriba = document.getElementById('btnArriba')
+if (btnArriba) {
+    window.addEventListener('scroll', () => {
+        btnArriba.style.display = window.scrollY > 300 ? 'block' : 'none'
+    })
+}
